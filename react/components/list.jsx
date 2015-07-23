254e1083e1090a9c5d94e1cf66693f0f2c222142
jffjs <jffreyjs@gmail.com>
2015-07-23 08:46:36 -0400

    get react to work

--- react/components/list.jsx
+++ react/components/list.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export class RecipeList extends React.Component {
+export default class RecipeList extends React.Component {
   render() {
     return <div>Hello world</div>;
   }
